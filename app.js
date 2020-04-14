ff4e184569bce4ff4450702331531822861ff9eb
vd89 <me@daxcodes.in>
2020-04-14 21:03:13 +0530

    the second Stage got the array and convert the value

--- app.js
+++ app.js
@@ -2,3 +2,34 @@ const readlineSync = require('readline-sync')
 
 const ascii = require('./ascii')
 
+// to get the input from the user 
+let input = readlineSync.question("Please provide the input letters:")
+
+let arr = input.split('')
+
+    // console.log(arr);
+
+const binaryConversion = (params) => {
+    if (params <=1) {
+        console.log(params);
+    } else {
+        binaryConversion(params / 2)
+        console.log(params%2);
+    }
+}
+
+const compare = (newArr) =>{
+    for (const i in newArr) {
+        if (newArr.hasOwnProperty(i)) {
+            let element = newArr[i]; 
+            let asciiValue = ascii.ascii[element]
+            console.log(asciiValue);
+            console.log(newArr);
+            
+        }
+    }
+}
+
+
+
+compare(arr)
