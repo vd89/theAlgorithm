6e316806d098b3cf5559ac495fb00dc620b6afd6
vd89 <me@daxcodes.in>
2020-04-15 06:15:08 +0530

    The output using the MAP method

--- app.js
+++ app.js
@@ -1,46 +1,44 @@
-const readlineSync = require('readline-sync')
+const readlineSync = require("readline-sync");
 
-const ascii = require('./ascii')
+const ascii = require("./ascii");
 
-// to get the input from the user 
-let input = readlineSync.question("Please provide the input letters:")
+// to get the input from the user
+let input = readlineSync.question("Please provide the input letters:");
 
-let arr = input.split('')
+let arr = input.split("");
 
-    // console.log(arr);
+// console.log(arr);
 
 let toBinary = (n) => {
-    let remArr = []
-    let resArr = []
-    if (n>=0) {
-        while (n) {
-            let q = parseInt(n / 2)
-            remArr.push(n % 2)
-            n = q            
-        }
-        for (let i = remArr.length - 1; i >= 0; i--){
-            resArr.push(remArr[i])
-        }
-        return parseInt(resArr.join(''))
-    } else {
-        return 
+  let remArr = [];
+  let resArr = [];
+  if (n >= 0) {
+    while (n) {
+      let q = parseInt(n / 2);
+      remArr.push(n % 2);
+      n = q;
     }
-}
-
-
-const compare = (newArr) =>{
-    for (const i in newArr) {
-        if (newArr.hasOwnProperty(i)) {
-            let element = newArr[i]; 
-            let asciiValue = ascii.ascii[element]
-            console.log(toBinary(asciiValue));
-            console.log(newArr)
-            
-        }
+    for (let i = remArr.length; i >= 0; i--) {
+      resArr.push(remArr[i]);
+      if (resArr[0] == undefined) {
+        resArr.shift();
+        resArr.unshift(0);
+      }
     }
-}
+    return resArr.join("");
+  } else {
+    return;
+  }
+};
 
 
 
-compare(arr)
-// binaryConversion(arr)
+const output = arr.map(item => {
+    const ascValue = ascii.ascii[item]
+    return (toBinary(ascValue))
+})
+
+console.log(output);
+
+// Please provide the input letters:aB cD
+//[ '01100001', '01000010', '0100000', '01100011', '01000100' ] 
